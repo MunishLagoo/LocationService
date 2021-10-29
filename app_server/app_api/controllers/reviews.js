83dafba921d2c6c76b7b38859218988fcd45fd9a
Munish Lagoo <munishlagoo@gmail.com>
2021-10-29 12:32:07 -0400

    Changes for a and b

--- app_server/app_api/controllers/reviews.js
+++ app_server/app_api/controllers/reviews.js
@@ -59,9 +59,80 @@ const reviewsDeleteOne = (req,res) => {
 };
 
 const reviewsCreate = (req,res) => {
-    res
-      .status(200)
-      .json({"status":"success"});
+    const locationId = req.params.locationid;
+    if(!locationId) {
+        Loc
+          .findById(locationId)
+          .select(review)
+          .exec((err, location) => {
+            if (err) {
+           return  res
+                  .status(404)
+                  .json(err);
+            } else {
+                doAddReview(req,res,location);
+            }
+          });
+    } else {
+ return  res
+         .status(404)
+         .json({"message":"Location not found...."});
+    }
+};
+
+const doAddReview = (req,res,location) => {
+  if (!location) {
+    return  res
+        .status(404)
+        .json({"message": "Location not found..."});
+  } else {
+      const {author, rating, reviewText} = req.body;
+      location.review.push({
+          author,
+          rating,
+          reviewText
+      });
+      location.save((err, location) => {
+          if(err) {
+           return  res
+                .status(400)
+                .json(err);
+          } else {
+            updateAverageRating(location._id);
+            const thisReview = location.reviews.slice(-1).pop();
+            res
+              .status(201)
+              .json(thisReview);
+          }
+      });
+  }
+};
+
+const updateAverageRating = (locationId) => {
+    Loc.findById(locationId)
+       .select('rating reviews')
+       .exec((err,location) => {
+        if (!err) {
+            doSetAverageRating(location);
+        }
+       });
+};
+
+const doSetAverageRating = (location) => {
+    if (location.reviews && location.reviews.length > 0) {
+        const count = location.reviews.length;
+        const total = location.reviews.reduce((acc, {rating}) => {
+            return acc + rating; }, 0);
+    
+    location.rating = parseInt(total/count,10);
+    location.save(err => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(`Average rating updated to $(location.rating}`);
+        }
+      });
+    }
 };
 
 module.exports = {
